c52557b9d1a8c2a47a72375e3afe8273dd04d754
Mansoor Ali <aliskepss@gmail.com>
2026-06-04 10:44:05 +0100

    Time: 0 ms (100%), Space: 20.6 MB (18.94%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -3,9 +3,6 @@ class Solution:
         seen = {}
         for i in range(len(nums)):
             complement = target - nums[i]
-
             if complement in seen:
                 return [seen[complement], i]
-
             seen[nums[i]] = i
-            
