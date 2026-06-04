f69e97a4b17aa8d26a03fb68699176c84ad81715
Mansoor Ali <aliskepss@gmail.com>
2026-06-04 10:28:45 +0100

    Time: 15 ms (43.14%), Space: 19.6 MB (23.51%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -2,11 +2,10 @@ class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         if len(s) != len(t):
             return False
-        word1 = {}
-        word2 = {}
+        dict1 = {}
+        dict2 = {}
         for i in range(len(s)):
-            word1[s[i]] = word1.get(s[i], 0) + 1
-        for j in range(len(t)):
-            word2[t[j]] = word2.get(t[j], 0) + 1
-        
-        return word1 == word2
+            dict1[s[i]] = dict1.get(s[i], 0) + 1
+        for i in range(len(t)):
+            dict2[t[i]] = dict2.get(t[i], 0) + 1
+        return dict1 == dict2
